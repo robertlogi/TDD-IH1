7a99537acee242b77de128aa4569abf0e40ab2ef
robertlogi <robertlogi15@gmail.com>
2023-02-26 22:51:33 +0000

    test sample 2

--- test_sample.py
+++ test_sample.py
@@ -5,3 +5,11 @@ def test_single_item_no_taxes_or_shipping():
     expected_price = 10
     assert calculate_base_price(items) == expected_price
     assert calculate_total_cost(items, tax=False, shipping=False) == expected_price
+
+def test_multiple_items_no_taxes_or_shipping():
+    items = [{"Name": "item1", "Price": 6, "Weight": 1.8, "Quantity": 2, "Location": "Germany"},
+             {"Name": "item2", "Price": 4, "Weight": 0.7, "Quantity": 3, "Location": "Colombia"},
+             {"Name": "item3", "Price": 9, "Weight": 2.0, "Quantity": 1, "Location": "Colombia"}]
+    expected_price = 33
+    assert calculate_base_price(items) == expected_price
+    assert calculate_total_cost(items, tax=False, shipping=False) == expected_price
