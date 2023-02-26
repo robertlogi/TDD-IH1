a85ef53deece0e682f29a921088d8c41948cbce9
robertlogi <robertlogi15@gmail.com>
2023-02-26 22:43:02 +0000

    test sample 1

--- test_sample.py
+++ test_sample.py
@@ -1,1 +1,7 @@
 from sample import *
+
+def test_single_item_no_taxes_or_shipping():
+    items = [{"Name": "item1", "Price": 10, "Weight": 0.5, "Quantity": 1, "Location": "Germany"}]
+    expected_price = 10
+    assert calculate_base_price(items) == expected_price
+    assert calculate_total_cost(items, tax=False, shipping=False) == expected_price
